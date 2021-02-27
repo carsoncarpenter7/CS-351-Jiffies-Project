4028a2ec8e1e9debeeac247c5c88de685986b19f
Carson Carpenter <carson.carpenter7@csu.fullerton.edu>
2021-02-26 20:58:09 -0700

    Jiffies Project

---
+++ hello.c
@@ -0,0 +1,42 @@
+/**
+ * hello.c
+ *
+ * Kernel module that communicates with /proc file system.
+ * 
+ * The makefile must be modified to compile this program.
+ * Change the line "simple.o" to "hello.o"
+ *
+ * Operating System Concepts - 10th Edition
+ * Copyright John Wiley & Sons - 2018
+ */
+
+// EXAMPLE CODE //
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/hash.h>
+#include <linux/gcd.h>
+
+/* This function is called when the module is loaded. */
+int simple_init(void)
+{
+       printk(KERN_INFO "Loading Module\n");
+       printk(KERN_INFO "GOLDEN_RATIO_PRIME IS: %lu\n", GOLDEN_RATIO_PRIME);
+       p
+       return 0;
+}
+
+/* This function is called when the module is removed. */
+void simple_exit(void) {
+       printk(KERN_INFO "Removing Module\n");
+       printk(KERN_INFO "gcd(3,300,24) is: %lu\n", gcd(3300,24));
+}
+
+/* Macros for registering module entry and exit points. */
+module_init( simple_init );
+module_exit( simple_exit );
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Simple Module");
+MODULE_AUTHOR("SGG");
---
+++ simple.c
@@ -0,0 +1,118 @@
+/**
+ * simple.c
+ *
+ * A simple kernel module. 
+ * 
+ * To compile, run makefile by entering "make"
+ *
+ * Operating System Concepts - 10th Edition
+ * Copyright John Wiley & Sons - 2018
+ */
+
+
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/proc_fs.h>
+#include <asm/uaccess.h>
+#include <linux/gcd.h>
+#include <linux/hash.h>
+#include <linux/jiffies.h>
+
+#define BUFFER_SIZE 128
+
+#define PROC_NAME "jiffies"
+
+
+unsigned long start;
+unsigned long end;
+unsigned long total;
+// #define MESSAGE "Hello World\n"
+
+/**
+ * Function prototypes (LEAVE)
+ */
+ssize_t proc_read(struct file *file, char *buf, size_t count, loff_t *pos);
+
+static struct file_operations proc_ops = {
+    .owner = THIS_MODULE,
+    .read = proc_read,
+};
+
+/* This function is called when the module is loaded. */
+int proc_init(void)
+{
+       if (strcmp(PROC_NAME, "jiffies") == 0) 
+       {
+              proc_create("jiffies", 0, NULL, &proc_ops);
+              printk(KERN_INFO "/proc/%s created\n", PROC_NAME);
+              printk(KERN_INFO "Jiffies is : %lu\n", jiffies);
+              printk("HZ is: %d\n", HZ);
+       }
+       else if (strcmp(PROC_NAME, "seconds") == 0)
+       {
+              proc_create(PROC_NAME, 0, NULL, &proc_ops);
+              start = jiffies;
+              printk(KERN_INFO "Jiffies start time: %lu\n", jiffies);
+       }
+       else
+       {
+              printk(KERN_INFO "Proc name cannot be found. Retry");
+       }
+       return 0;
+}
+
+/* This function is called when the module is removed. */
+void proc_exit(void)
+{
+       printk(KERN_INFO "/proc/%s removed\n", PROC_NAME);
+       printk(KERN_INFO "Jiffies end time: %lu\n", jiffies);
+
+       end = jiffies;
+       total = end - start;
+
+       printk(KERN_INFO "Total time: %lu seconds \n", total / HZ);
+       //removes the /proc/hello entry
+       remove_proc_entry(PROC_NAME, NULL);
+       printk(KERN_INFO "/proc/%s removed\n", PROC_NAME);
+}
+
+ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, loff_t *pos)
+{
+       int rv = 0;
+       char buffer[BUFFER_SIZE];
+       static int completed = 0;
+
+       if (completed)
+       {
+              completed = 0;
+              return 0;
+       }
+
+       completed = 1;
+
+       if (strcmp(PROC_NAME, "jiffies") == 0)
+       {
+              rv = sprintf(buffer, "Jiffies current values is:%lu\n", jiffies);
+       }
+       else
+       {
+              rv = sprintf(buffer, "Seconds is calculated after proc is closed and jiffies is: %lu\n", jiffies);
+       }
+       // Reference
+       //rv = sprintf(buffer, "Hello World\n");
+
+       // copies the contents of buffer to userspace usr_buf
+       copy_to_user(usr_buf, buffer, rv);
+
+       return rv;
+}
+
+/* Macros for registering module entry and exit points. */
+module_init(proc_init);
+module_exit(proc_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Display Seconds");
+MODULE_AUTHOR("SGG");
